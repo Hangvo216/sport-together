1c290513e29d066f6d8f30f256af945482518b9e
Nguyen Nguyen <nguyen@Nguyens-MacBook-Pro.local>
2016-04-22 01:44:35 -0400

    Add header, add team profile.

--- public_html/team-profile.php
+++ public_html/team-profile.php
@@ -2,9 +2,12 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Sport together</title>
 </head>
+
+
 <body>
+<?php require "header.php"?>
 
    <div class="container">
 
@@ -20,11 +23,20 @@
        <textarea class="team-description" rows="4" cols="50">
 	</textarea>
 	<button type="submit" class="btn btn-default" id="save-description">Save</button>
+	<div class="team-buttons">
+		<button type="submit" class="btn btn-default" id="make-game">Tao tran dau</button>
+		<button type="submit" class="btn btn-default" id="join-team">tham gia doi bong </button>
+	</div>
+	
+	<!-- div looking for team -->
+      <div class="find-team container">
             
 			<div class="row">
 				<div class="col-md-6 hero-feature">
 	                <div class="thumbnail">
 	                    <div class="info">
+	                    	<label><h3>Dang tim doi banh</h3>
+	                    	</label>
 	                    	<p class="text-right"> The loai: <span>5 vs 5</span></p>
 	                        <p>Doi: <span><a href="#">Sport Together</a></span></p>
 							<p>Dia diem: <span> Quan 7</span></p>
@@ -37,11 +49,16 @@
 	                </div>
 	            </div>
             </div>
+     </div>
+     
+     <div class="scheduled-game container">
             
 			<div class="row">
 				<div class="col-md-6 hero-feature">
 	                <div class="thumbnail">
 	                    <div class="info">
+	                    <label><h3>tran dau sap den</h3>
+	                    	</label>
 	                        <p class="text-right"> The loai: <span>5 vs 5</span></p>
 	                        <p>Doi: <span><a href="#">Sport Together 2</a></span></p>
 							<p>Dia diem: <span> Quan 7</span></p>
@@ -54,8 +71,29 @@
 	                </div>
 	            </div>
             </div>
-		
-			
+	</div>
+	
+	<div class="played-already container">
+            
+			<div class="row">
+				<div class="col-md-6 hero-feature">
+	                <div class="thumbnail">
+	                    <div class="info">
+	                    <label><h3>Tran dau da choi</h3>
+	                    	</label>
+	                        <p class="text-right"> The loai: <span>5 vs 5</span></p>
+	                        <p>Doi: <span><a href="#">Sport Together 2</a></span></p>
+							<p>Dia diem: <span> Quan 7</span></p>
+							<p>Ngay: <span> April 17</span></p>
+							<p>Thoi gian: <span> 3 - 5 pm</span></p>
+							<p class="text-right"><span><a href="#"> Chi tiet</a></span></p>
+							<button type="submit" class="btn btn-default">Tham gia</button>
+					
+	                    </div>
+	                </div>
+	            </div>
+            </div>
+	</div>
 
       
  </div>
